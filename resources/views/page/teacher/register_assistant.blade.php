ec7f7a2c5874147ff8555e2ad498fab93c3e562e
JingWangTW <00457008@email.ntou.edu.tw>
2018-09-05 21:45:26 +0800

    blank account fix

--- resources/views/page/teacher/register_assistant.blade.php
+++ resources/views/page/teacher/register_assistant.blade.php
@@ -2,11 +2,28 @@
 
 @section('title', 'Register A New Assistant')
 
+@section('custom_js')
+    
+    function check () {
+        
+        if ( $('#name').val().trim().length && $('#email').val().trim().length ) {
+            
+            return true;
+        }
+        else {
+            $('#WrongInput').show();
+            return false;
+        }
+    }
+    
+@endsection
+
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <form class="col-md-10 col-sm-12 col-12" action="/api/add_assistant" method="POST">
+            <form class="col-md-10 col-sm-12 col-12" action="/api/add_assistant" method="POST" onsubmit="return check();">
                 <h1 class="text-muted">Register A New Assistant</h1>
+                <span class="text-danger" id="WrongInput" style="display: none"> Name and email can't be blank. </span>
                 <div class="form-group">
                     <label for="name">Assistant Name</label>
                     <input type="text" class="form-control" id="name" placeholder="Name" name="name" required>
--- resources/views/page/teacher/register_teacher.blade.php
+++ resources/views/page/teacher/register_teacher.blade.php
@@ -2,11 +2,28 @@
 
 @section('title', 'Register A New Teacher')
 
+@section('custom_js')
+    
+    function check () {
+        
+        if ( $('#name').val().trim().length && $('#email').val().trim().length ) {
+            
+            return true;
+        }
+        else {
+            $('#WrongInput').show();
+            return false;
+        }
+    }
+    
+@endsection
+
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <form class="col-md-10 col-sm-12 col-12" action="/api/add_teacher" method="POST">
+            <form class="col-md-10 col-sm-12 col-12" action="/api/add_teacher" method="POST" onsubmit="return check();">
                 <h1 class="text-muted"> Register A New Teacher </h1>
+                <span class="text-danger" id="WrongInput" style="display: none"> Name and email can't be blank. </span>
                 <div class="form-group">
                     <label for="name">Name</label>
                     <input type="text" class="form-control" id="name" placeholder="Name" name="name" required>
